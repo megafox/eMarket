4f93c8dcf4e8755c1ada31638c1bff7a7437cf89
megafox <argie.badua@gmail.com>
2015-06-15 09:49:15 +0800

    eMarket

---
+++ ads.php
@@ -0,0 +1,21 @@
+<div class='search-form'>
+	<form>
+		<input type='text' value='Search..'>
+		<input type='submit' value=' ' />
+	</form>
+</div>
+<div class='clear'> </div>
+<div class='ads-section'>
+	
+		<div class='ads-grid'>
+			<div class='ads-pic-grid'>
+				<img class='ads-pic' src='images/chat-people-pic1.png' title='chat-people-name' />		
+			</div>
+			<div class='ads-message'>
+				<h3><a href='products.php'>Samsung Galaxy Grand Prime</a></h3>
+				<p>Php 1,000 <span> per pair</span></p>
+				<small>Ilocos Norte | 2015.05.30 12:45p</small>
+			</div>
+			<div class='clear'> </div>
+		</div>
+</div>
---
+++ inbox.php
@@ -0,0 +1,92 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.1//EN" "http://www.w3.org/TR/xhtml11/DTD/xhtml11.dtd">
+<html>
+	<head>
+		<title>App Pinoy</title>
+		<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
+		<link rel="shortcut icon" type="image/x-icon" href="images/fav-icon.png" />
+		<link href="css/style2.css" rel="stylesheet" type="text/css"  media="all" />
+		<meta name="keywords" content="" />
+	</head>
+	<body>
+		<div class="whole" style="width: 100%; height: 100%;">
+			<div class="header-line" style="position: fixed; top: 0em; left: 0px; background:url(images/body-border.png); height: 0.25em; width: 100%; z-index: 100;"></div>
+					<div class="wrap">	
+						<div class="msg-section">
+							
+								<div class="msg-grid">
+									<div class="msg-pic-grid">
+										<img class="msg-pic" src="images/chat-people-pic1.png" title="chat-people-name" />		
+									</div>
+									<div class="msg-message">
+										<h3><a href="message.php">Arghs</a></h3>
+										<p>This is the quick message..</p>
+										<small>Ilocos Norte | 2015.05.30 12:45p</small>
+									</div>
+									<div class="clear"> </div>
+								</div>
+								<div class="msg-grid">
+									<div class="msg-pic-grid">
+										<img class="msg-pic" src="images/chat-people-pic1.png" title="chat-people-name" />		
+									</div>
+									<div class="msg-message">
+										<h3><a href="message.php">Arghs</a></h3>
+										<p>This is the quick message..</p>
+										<small>Ilocos Norte | 2015.05.30 12:45p</small>
+									</div>
+									<div class="clear"> </div>
+								</div>
+								<div class="msg-grid">
+									<div class="msg-pic-grid">
+										<img class="msg-pic" src="images/chat-people-pic1.png" title="chat-people-name" />		
+									</div>
+									<div class="msg-message">
+										<h3><a href="message.php">Arghs</a></h3>
+										<p>This is the quick message..</p>
+										<small>Ilocos Norte | 2015.05.30 12:45p</small>
+									</div>
+									<div class="clear"> </div>
+								</div>
+								<div class="msg-grid">
+									<div class="msg-pic-grid">
+										<img class="msg-pic" src="images/chat-people-pic1.png" title="chat-people-name" />		
+									</div>
+									<div class="msg-message">
+										<h3><a href="message.php">Arghs</a></h3>
+										<p>This is the quick message..</p>
+										<small>Ilocos Norte | 2015.05.30 12:45p</small>
+									</div>
+									<div class="clear"> </div>
+								</div>
+								<div class="msg-grid">
+									<div class="msg-pic-grid">
+										<img class="msg-pic" src="images/chat-people-pic1.png" title="chat-people-name" />		
+									</div>
+									<div class="msg-message">
+										<h3><a href="message.php">Arghs</a></h3>
+										<p>This is the quick message..</p>
+										<small>Ilocos Norte | 2015.05.30 12:45p</small>
+									</div>
+									<div class="clear"> </div>
+								</div>
+								<div class="msg-grid">
+									<div class="msg-pic-grid">
+										<img class="msg-pic" src="images/chat-people-pic1.png" title="chat-people-name" />		
+									</div>
+									<div class="msg-message">
+										<h3><a href="message.php">Arghs</a></h3>
+										<p>This is the quick message..</p>
+										<small>Ilocos Norte | 2015.05.30 12:45p</small>
+									</div>
+									<div class="clear"> </div>
+								</div>
+								<br/><br/><br/><br/>
+						</div>
+					</div>
+			</div>
+
+
+		<div class="footer-logo" style="position: fixed; bottom: 0em; left: 0px; background:url(images/body-border.png); height: 0.25em; width: 100%;"></div>
+ 		<div class="footer-line" style="position: fixed; bottom: 0.25em; left: 0px; background-color: #70389c; height: 3em; width: 100%;">
+ 			
+ 		</div>
+ 	</div>
